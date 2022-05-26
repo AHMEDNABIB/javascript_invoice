9b9e5a8674fd185ec4e50da1cff2110587432c09
AHMED NABIB <nabibpallab@gmail.com>
2022-05-27 00:39:49 +0600

    add and display item complete

--- app.js
+++ app.js
@@ -8,3 +8,43 @@ detailsButton.addEventListener("click", function (params) {
 
 	buyerDetails.value = "";
 });
+
+const addProductBtn = document.getElementById("add-details-btn");
+
+addProductBtn.addEventListener("click", function (params) {
+    const infoTable = document.getElementById("info-table");
+    console.log(infoTable)
+	const itemName = document.getElementById("item-name-input");
+	const itemPrice = document.getElementById("item-price-input");
+	const itemQuantity = document.getElementById("item-quantity-input");
+
+	// console.log(itemName.value, itemPrice.value, itemQuantity.value);
+
+	{
+		// <tr>
+		// 	<th>1</th>
+		// 	<td>Mark</td>
+		// 	<td>Otto</td>
+		// 	<td>@mdo</td>
+		// </tr>;
+	}
+    
+    const totalPrice = parseInt(itemPrice.value) * parseInt(itemQuantity.value)
+	const tr = document.createElement("tr");
+	const th = document.createElement("th");
+	const td1 = document.createElement("td");
+	const td2 = document.createElement("td");
+	const td3 = document.createElement("td");
+
+	th.innerText = itemName.value;
+    td1.innerText = itemPrice.value;
+    td2.innerText = itemQuantity.value;
+    td3.innerText = totalPrice
+
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    infoTable.appendChild(tr);
+
+});
